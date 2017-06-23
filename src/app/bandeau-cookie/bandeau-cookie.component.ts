c4251a4705550e002814e73e8b790a68839a4ece
Jamatofu <clement.beal96@gmail.com>
2017-06-23 19:21:55 +0200

    Création d'un cookie pour se souvenir que l'utilisateur est d'accord avec la loi des cookies

--- src/app/bandeau-cookie/bandeau-cookie.component.ts
+++ src/app/bandeau-cookie/bandeau-cookie.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Cookie} from "ng2-cookies";
 
 @Component({
   selector: 'app-bandeau-cookie',
@@ -7,15 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BandeauCookieComponent implements OnInit {
   public isOk: boolean;
+  public yetAccepted: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.isYetAccepted();
   }
 
   // TODO Faire en sorte qu'on crée un cookie
 
   public fermerBandeau() {
     this.isOk = !this.isOk;
+    Cookie.set("acceptedLawCondition", "true", 7);
+  }
+
+  public isYetAccepted() {
+    console.log(Cookie.check("acceptedLawCondition"));
+    this.yetAccepted = Cookie.check("acceptedLawCondition");
   }
 }
