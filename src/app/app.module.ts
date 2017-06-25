2a96d63b47ac985c90aed95fd23a803cdbf0f87a
Jamatofu <clement.beal96@gmail.com>
2017-06-25 15:49:51 +0200

    Déclaration des routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { BandeauCookieComponent } from './bandeau-cookie/bandeau-cookie.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {RouterModule, Routes} from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'accueil', component: ContactComponent },
+  { path: '',   redirectTo: '/accueil', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
     BandeauCookieComponent,
-    NavbarComponent
+    NavbarComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
